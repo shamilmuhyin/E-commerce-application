bb79f22b7dc7d40f3807d05a1268470114c914cb
shamilmuhyin <shamilmuhyin@gmail.com>
2023-01-28 07:28:08 +0530

    first backend code

---
+++ E-commerce-application-backend/src/main/java/com/tomato/grocery/ECommerceApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ E-commerce-application-backend/src/test/java/com/tomato/grocery/ECommerceApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
